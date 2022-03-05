7a65d140139a37d81cdb46c7951f72bb0e2b68df
Priyanshu Singh <priyanshusingh0610@gmail.com>
2022-03-05 22:51:52 +0530

    CRYPTMUSIC

--- src/utils/ytdata.js
+++ src/utils/ytdata.js
@@ -1,38 +1,62 @@
 const request=require('request')
 const iplocation=require('./iplocation')
 
-const ytdata=(req, query, callback)=>{
-    let url=`https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_KEY}&q=${encodeURIComponent(query)}&part=snippet&maxResults=10`
-    const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-    iplocation(ipaddress, (error, body)=>{
-        if (!error) {
-            url+=`&location=${body.lat}%2C%20${body.lon}&locationRadius=500km&type=video%2Clist`
+const ytdata=(query, callback)=>{
+    const url=`https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_KEY}&q=${encodeURIComponent(query)}&part=snippet&maxResults=10`
+    // const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    request({url, json: true}, (error, {body})=>{
+        if (error) {
+            callback('Unable to connect to YT API', undefined)
+        } else if (body.error) {
+            callback('Limit exceeded', undefined)
         }
-        request({url, json: true}, (error, {body})=>{
-            if (error) {
-                callback('Unable to connect to YT API', undefined)
-            } else if (body.error) {
-                callback('Limit exceeded', undefined)
-            }
-            else if (body.items.length===0) {
-                callback('No match found!', undefined)
-            } else {
-                let result=[]
-                body.items.forEach((data)=>{
-                    result.push({
-                        videoId: data.id.videoId,
-                        publishedAt: data.snippet.publishedAt,
-                        title: data.snippet.title,
-                        description: data.snippet.description,
-                        thumbnails: data.snippet.thumbnails,
-                        channelTitle: data.snippet.channelTitle,
-                        publishTime: data.snippet.publishTime
-                    })
+        else if (body.items.length===0) {
+            callback('No match found!', undefined)
+        } else {
+            let result=[]
+            body.items.forEach((data)=>{
+                result.push({
+                    videoId: data.id.videoId,
+                    publishedAt: data.snippet.publishedAt,
+                    title: data.snippet.title,
+                    description: data.snippet.description,
+                    thumbnails: data.snippet.thumbnails,
+                    channelTitle: data.snippet.channelTitle,
+                    publishTime: data.snippet.publishTime
                 })
-                callback(undefined, result)
-            }
-        })
+            })
+            callback(undefined, result)
+        }
     })
+    // iplocation(ipaddress, (error, body)=>{
+    //     if (!error) {
+    //         url+=`&location=${body.lat}%2C%20${body.lon}&locationRadius=500km&type=video%2Clist`
+    //     }
+    //     request({url, json: true}, (error, {body})=>{
+    //         if (error) {
+    //             callback('Unable to connect to YT API', undefined)
+    //         } else if (body.error) {
+    //             callback('Limit exceeded', undefined)
+    //         }
+    //         else if (body.items.length===0) {
+    //             callback('No match found!', undefined)
+    //         } else {
+    //             let result=[]
+    //             body.items.forEach((data)=>{
+    //                 result.push({
+    //                     videoId: data.id.videoId,
+    //                     publishedAt: data.snippet.publishedAt,
+    //                     title: data.snippet.title,
+    //                     description: data.snippet.description,
+    //                     thumbnails: data.snippet.thumbnails,
+    //                     channelTitle: data.snippet.channelTitle,
+    //                     publishTime: data.snippet.publishTime
+    //                 })
+    //             })
+    //             callback(undefined, result)
+    //         }
+    //     })
+    // })
 }
 
 module.exports=ytdata
