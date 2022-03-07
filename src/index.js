904d134d4f525457a2ff22c7d39d87c7ff02db0b
Priyanshu Singh <priyanshusingh0610@gmail.com>
2022-03-07 12:46:54 +0530

    CRYPTMUSIC

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const userRouter=require('./routers/user')
 const playlistRouter=require('./routers/playlist')
 const songRouter=require('./routers/song')
 const searchRouter=require('./routers/search')
+const playerRouter=require('./routers/player')
 
 const app=express()
 var expressWs = require('express-ws')(app);
@@ -17,6 +18,7 @@ app.use(userRouter)
 app.use(playlistRouter)
 app.use(songRouter)
 app.use(searchRouter)
+app.use(playerRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is up on port ${port}`)
---
+++ src/routers/player.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const ytdl=require('ytdl-core')
+
+const router=express.Router()
+
+router.get('/player', (req, res)=>{
+    const v=req.query.v
+    if (!v) {
+        return res.status(400).send()
+    }
+    try {
+        ytdl(`https://www.youtube.com/watch?v=${v}`, {
+        quality: 'highestaudio',
+        filter: 'audioonly'
+    }).pipe(res)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+module.exports=router
