c0bcfd1a63e08c0557a250e64e5cf93ea7d43f60
Priyanshu Singh <priyanshusingh0610@gmail.com>
2022-03-18 21:08:42 +0530

    CRYPTMUSIC

--- src/models/user.js
+++ src/models/user.js
@@ -49,7 +49,7 @@ const userSchema=mongoose.Schema({
 
 userSchema.methods.generateAuthToken=async function() {
     const user=this
-    const token=jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET, {expiresIn: '30 days'})
+    const token=jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET)
     user.tokens=user.tokens.concat({token})
     await user.save()
     return token
